40be9ca3c826eb66a26dda66f9a3a1158a87354f
Santhosh K S <santhoshachar08@gmail.com>
2023-11-13 21:25:55 +0100

    Seed and stacking elements on top

--- svgFilterExperiments/src/StarFilter.tsx
+++ svgFilterExperiments/src/StarFilter.tsx
@@ -1,7 +1,7 @@
 export default function StarFilter() {
   return (
       <filter id="starFilter">
-        <feTurbulence baseFrequency="0.8"/>
+        <feTurbulence baseFrequency="0.8" seed="2"/>
         <feColorMatrix values=" 0.5 0 0 9 -5 
                                 0 0.3 0 9 -5
                                 0 0 1 9 -5 
--- svgFilterExperiments/src/SvgFilter.tsx
+++ svgFilterExperiments/src/SvgFilter.tsx
@@ -3,9 +3,11 @@ import StarFilter from "./StarFilter"
 export default function SvgFilter() {
 
   return (
-  <svg width="100%" height="100%">
+  <svg width="100%" height="100%" viewBox="0 0 500 100">
       <StarFilter/>
       <rect width="100%" height="100%" filter="url(#starFilter)"/>
+      <StarFilter/>
+      <rect x="10" y="10" width="30%" height="30%"  fill="red"/>
   </svg>
   );
 
